94b45bcc5cefcba556792c1b23b81a947a7adc76
Norman Fomferra <norman.fomferra@gmail.com>
2023-06-01 10:45:23 +0200

    new CLI command "info"

--- xcube_smos/nckcindex/cli.py
+++ xcube_smos/nckcindex/cli.py
@@ -46,7 +46,7 @@ def create(ctx,
     """Create a NetCDF Kerchunk index."""
     from xcube_smos.nckcindex.nckcindex import NcKcIndex
     # click.echo(f"Debug is {'on' if ctx.obj['DEBUG'] else 'off'}")
-    NcKcIndex.create(
+    nc_kc_index = NcKcIndex.create(
         index_urlpath=index,
         s3_bucket=bucket,
         s3_options={
@@ -54,6 +54,7 @@ def create(ctx,
             "key": key or os.environ.get("AWS_ACCESS_KEY_IDENTIFIER"),
             "secret": secret or os.environ.get("AWS_SECRET_ACCESS_KEY"),
         })
+    print(f"Created {nc_kc_index.index_path}")
 
 
 @cli.command()
@@ -63,18 +64,44 @@ def create(ctx,
                    f' Defaults to "{DEFAULT_INDEX_NAME}".')
 @click.option('--prefix', metavar='<path>',
               help='S3 key prefix')
+@click.option('--force', is_flag=True,
+              help='Do not skip existing indexes.')
 @click.option('--dry-run', is_flag=True,
-              help='Do not write any files.')
+              help='Do not create any indexes.')
 @click.pass_context
 def sync(ctx,
          index,
          prefix,
+         force,
          dry_run):
     """Synchronize a NetCDF Kerchunk index."""
     from xcube_smos.nckcindex.nckcindex import NcKcIndex
     # click.echo(f"Debug is {'on' if ctx.obj['DEBUG'] else 'off'}")
-    nc_file_index = NcKcIndex.open(index_urlpath=index)
-    nc_file_index.sync(prefix=prefix, dry_run=dry_run)
+    nc_kc_index = NcKcIndex.open(index_urlpath=index)
+    num_files = nc_kc_index.sync(prefix=prefix, force=force, dry_run=dry_run)
+    print(f"{num_files} file(s) synchronized in {nc_kc_index.index_path}")
+
+
+@cli.command()
+@click.option('--index', nargs=1, metavar='<path>',
+              default=DEFAULT_INDEX_NAME,
+              help=f'Local index directory path. Must exist.'
+                   f' Defaults to "{DEFAULT_INDEX_NAME}".')
+@click.pass_context
+def info(ctx, index):
+    """Inform about a NetCDF Kerchunk index."""
+    from xcube_smos.nckcindex.nckcindex import NcKcIndex
+    # click.echo(f"Debug is {'on' if ctx.obj['DEBUG'] else 'off'}")
+    nc_kc_index = NcKcIndex.open(index_urlpath=index)
+    print(f"Index path: {os.path.abspath(nc_kc_index.index_path)}")
+    print(f"S3 endpoint URL: {nc_kc_index.s3_endpoint_url}")
+    print(f"S3 bucket: {nc_kc_index.s3_bucket}")
+    if nc_kc_index.s3_prefixes:
+        print("S3 prefixes:")
+        for k, v in nc_kc_index.s3_prefixes.items():
+            print(f"  {k}: {v}")
+    else:
+        print("No defined S3 prefixes.")
 
 
 if __name__ == '__main__':
