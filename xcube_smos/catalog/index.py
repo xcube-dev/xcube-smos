cd191bc0df5f4fde90ac868fca2ca3ddd157dd39
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2023-12-02 12:27:08 +0100

    fix kwarg names

--- xcube_smos/catalog/index.py
+++ xcube_smos/catalog/index.py
@@ -42,25 +42,25 @@ _ONE_DAY = pd.Timedelta(1, unit="days")
 class SmosIndexCatalog(AbstractSmosCatalog):
     """A SMOS L2 dataset catalog that uses a Kerchunk index (NcKcIndex).
 
-    :param index_urlpath: Path or URL to the root directory
-    :param index_options: Storage options to access *index_urlpath*.
+    :param index_path: Path or URL to the root directory
+    :param index_storage_options: Storage options to access *index_urlpath*.
     """
 
     def __init__(self,
-                 index_urlpath: Optional[Union[str, Path]] = None,
-                 index_options: Optional[Dict[str, Any]] = None):
-        index_urlpath = index_urlpath or os.environ.get(INDEX_ENV_VAR_NAME)
-        assert_given(index_urlpath, name='index_urlpath')
-        index_urlpath = os.path.expanduser(str(index_urlpath))
-        self._nc_kc_index = NcKcIndex.open(index_urlpath,
-                                           index_options=index_options)
+                 index_path: Optional[Union[str, Path]] = None,
+                 index_storage_options: Optional[Dict[str, Any]] = None):
+        index_path = index_path or os.environ.get(INDEX_ENV_VAR_NAME)
+        assert_given(index_path, name='index_urlpath')
+        index_path = os.path.expanduser(str(index_path))
+        self._nc_kc_index = NcKcIndex.open(index_path=index_path,
+                                           index_storage_options=index_storage_options)
 
     @property
     def dataset_opener(self) -> DatasetOpener:
         return SmosIndexCatalog.open_dataset
 
     @staticmethod
-    def open_dataset(dataset_path: str, remote_storage_options: dict) \
+    def open_dataset(dataset_path: str, source_storage_options: dict) \
             -> xr.Dataset:
         index_filename = dataset_path.rsplit("/", maxsplit=1)[-1]
         index_json_path = f"{dataset_path}/{index_filename}.nc.json"
@@ -71,7 +71,7 @@ class SmosIndexCatalog(AbstractSmosCatalog):
                 "storage_options": {
                     "fo": index_json_path,
                     "remote_protocol": "s3",
-                    "remote_options": remote_storage_options or {}
+                    "remote_options": source_storage_options or {}
                 },
                 "consolidated": False
             },
